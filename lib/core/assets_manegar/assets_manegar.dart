1a7a527309ef077e96848bb12df93bd57070d064
Kassem Diaa <kassemdiaa537@gmail.com>
2026-02-24 00:41:29 +0200

    finish assets manegar data

---
+++ lib/core/assets_manegar/assets_manegar.dart
@@ -0,0 +1,35 @@
+abstract class AssetsManegar {
+  static const String mathVid = 'assets/videos/math.mp4';
+
+  static const String mathdoc = 'assets/docs/math.pdf';
+
+  static const String bookIcon = 'assets/icons/bookicon.png';
+  static const String barinIcon = 'assets/icons/brainlogo.png';
+  static const String certificateIcon = 'assets/icons/certificateicon.png';
+  static const String circledPlayIcon = 'assets/icons/circledPlay.png';
+  static const String clockIcon = 'assets/icons/clockicon.png';
+  static const String homeIcon = 'assets/icons/homelogo.png';
+  static const String notificationIcon = 'assets/icons/notificationlogo.png';
+  static const String offarIcon = 'assets/icons/offoricon.png';
+  static const String profileIcon = 'assets/icons/profilelogo.png';
+  static const String docIcon = 'assets/icons/viewdetails.png';
+
+  static const String arabic = 'assets/images/arabic.jpg';
+  static const String arabicCover = 'assets/images/arabiccover.jpg';
+  static const String chemistry = 'assets/images/chimstry.jpg';
+  static const String chemistryCover = 'assets/images/chimstrycover.jpg';
+  static const String english = 'assets/images/english.jpg';
+  static const String englishCover = 'assets/images/englishcover.jpg';
+  static const String french = 'assets/images/french.jpg';
+  static const String frenchCover = 'assets/images/frenchcover.jpg';
+  static const String math = 'assets/images/math.jpg';
+  static const String mathCover = 'assets/images/mathcover.jpg';
+  static const String physics = 'assets/images/physics.jpg';
+  static const String physicsCover = 'assets/images/physicscover.jpg';
+
+  static const String loginOrRegister = 'assets/svgs/loginorregister.svg';
+  static const String onbourdingOne = 'assets/svgs/onbourdingone.svg';
+  static const String onbourdingTwo = 'assets/svgs/onbourdingtwo.svg';
+  static const String onbourdingThree = 'assets/svgs/onbourdingthree.svg';
+  static const String onbourdingFour = 'assets/svgs/onbourdingfour.svg';
+}
